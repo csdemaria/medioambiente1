5ae5f9686bdaca7969a68f94bbe134dee3bb9c5f
csdemaria <celestesofiademaria@gmail.com>
2022-09-22 19:33:37 -0300

    primeros prompts en javascript

---
+++ main.js
@@ -0,0 +1,28 @@
+//Bienvenida
+
+let nombre = prompt("Ingrese su nombre.");
+
+let saludo = "Bienvenido " + nombre + " a nuestro sitio.";
+
+alert(saludo);
+
+
+// Pregunta inicial
+
+let preguntainicial = prompt("Te interesa el reciclaje?");
+
+let afirmacion = "si";
+
+if(preguntainicial == afirmacion){
+    //devuelve true
+    alert("Genial! Esperemos que disfrutes el contenido de nuestra página.")
+} else {
+    //devuelve false
+    alert("Tal vez leyendo la información en esta página despierte tu interés por el reciclaje.")
+}
+
+
+
+//Localidad
+
+// let localidad = prompt("De qué localidad sos?");
