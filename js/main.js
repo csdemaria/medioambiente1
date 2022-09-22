b2ada955b4f09c7ea3ad0591b194a9fc6a77f756
rocioalbarino <100244315+rocioalbarino@users.noreply.github.com>
2022-09-22 20:23:19 -0300

    Implementacion js en index

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector( 'h1');
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Bienvenida,' + miNombre;
+}
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    let nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Bienvenida, ' + nombreAlmacenado;
+}
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Introduzca su nombre.');
+    if(!miNombre) {
+      estableceNombreUsuario();
+    } else {
+      localStorage.setItem('nombre', miNombre);
+      miTitulo.innerHTML = 'Bienvenida, ' + miNombre;
+    }
+  }
