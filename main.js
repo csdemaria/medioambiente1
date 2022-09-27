83a645be6c464b02806fc43f96995558734f28b2
csdemaria <celestesofiademaria@gmail.com>
2022-09-27 20:10:56 -0300

    botón y cálculo de multiplicación

--- main.js
+++ main.js
@@ -21,5 +21,57 @@ if(preguntainicial == afirmacion){
     alert("Tal vez leyendo la información en esta página despierte tu interés por el reciclaje.")
 }
 
+// let numeroDias = prompt("Ingrese una cantidad de días");
 
+// let numValor = 0.79;
+
+// let respuestaCalculo = parseInt(numberRequest) * numValor;
+
+// let resultado = 0;
+// let operacion = prompt("Ingrese la operación");
+// let numeroA = prompt("Ingrese un número");
+// let numeroB = prompt("Ingrese un número");
+    
+
+// function calcular(operacion, numeroA, numeroB) {
+
+    
+//     switch (operacion) {
+//         case "sumar":
+//             resultado = numeroA + numeroB
+//             break;
+//         case "restar":
+//             resultado = numeroA - numeroB
+//             break;
+//         case "multiplicar":
+//             resultado = numeroA * numeroB
+//             break;
+//         case "dividir":
+//             resultado = numeroA / numeroB
+//             break;
+//         default:
+//             resultado = 0
+//             break;
+//     }
+//     alert("El resultado de la operación es: " + resultado)
+// }
+
+// let resultado = 0;
+// let numeroA = prompt("Ingrese un número de días");
+// let numeroB = 0.79;
+    
+
+// function calcular(numeroA, numeroB) {
+
+//      resultado = numeroA * numeroB;
+    
+//     alert("El resultado de la operación es: " + resultado)
+// }
+
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = 0.79;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
 
