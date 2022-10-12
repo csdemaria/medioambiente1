a5a75346b27d0f77489dfa413a04097fbe8366d5
csdemaria <celestesofiademaria@gmail.com>
2022-10-11 21:32:12 -0300

    links menú pag nueva llevar + array localidades

--- llevar.js
+++ llevar.js
@@ -1,6 +1,15 @@
 //Elegir Localidad
 
-function localidadElegida() {
-    var x = document.getElementById("localidad").value;
-    document.getElementById("lugarElegido").innerHTML = x;
+let localidades = ["Abasto", "City Bell", "El Peligro", "Joaquin Gorina", "Lisandro Olmos", "La Plata(Capital)", "Los Hornos", "Manuel B. Gonnet", "Melchor Romero", "Ringuelet", "Tolosa", "Villa Elvira", "Villa Elisa"]
+
+for (const localidad of localidades) {
+  let elementOption = document.createElement("OPTION");
+  elementOption.innerText = localidad;
+  document.getElementById("localidades").appendChild(elementOption)
   }
+
+
+function localidadElegida() {
+  var x = document.getElementById("localidades").value;
+  document.getElementById("lugarElegido").innerHTML = x;
+}
