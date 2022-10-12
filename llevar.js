45280c25ce73f758daab6171f9b1a543be559cb1
csdemaria <celestesofiademaria@gmail.com>
2022-10-12 01:18:26 -0300

    listado de puntos verdes y agregado el mapa

--- llevar.js
+++ llevar.js
@@ -1,6 +1,6 @@
 //Elegir Localidad
 
-let localidades = ["Abasto", "City Bell", "El Peligro", "Joaquin Gorina", "Lisandro Olmos", "La Plata(Capital)", "Los Hornos", "Manuel B. Gonnet", "Melchor Romero", "Ringuelet", "Tolosa", "Villa Elvira", "Villa Elisa"]
+let localidades = ["City Bell", "La Plata(Capital)", "Manuel B. Gonnet", "Tolosa", "Villa Elisa"]
 
 for (const localidad of localidades) {
   let elementOption = document.createElement("OPTION");
@@ -10,6 +10,29 @@ for (const localidad of localidades) {
 
 
 function localidadElegida() {
-  var x = document.getElementById("localidades").value;
-  document.getElementById("lugarElegido").innerHTML = x;
+  let puntosVerdes = document.getElementById("localidades").value;
+  document.getElementById("lugarElegido").innerHTML = puntosVerdes;
+
+  switch (puntosVerdes) {
+
+    case "City Bell":
+      document.getElementById("lugarElegido").innerHTML = ["Plaza Belgrano"];
+      break;
+
+    case "La Plata(Capital)":
+      document.getElementById("lugarElegido").innerHTML = [" Parque Alberti (24 y 39) ", " Parque Castelli (24 y 67) ", " Plaza Alsina (1 y 38) ", " 72 y 28 (Altos de San Lorenzo) ", " Plaza Matheu (1 y 66) ", " Casa Ecológica del Paseo del Bosque ", " Dirección de Tratamiento de Residuos (49 y 21) ", " 652 y 22 (Villa Parque Sicardi) "];
+      break;
+
+    case "Manuel B. Gonnet":
+      document.getElementById("lugarElegido").innerHTML = [" Delegación comunal ubicada en 495 y 15 bis ", " Parque Boveri "];
+      break;
+
+    case "Tolosa":
+      document.getElementById("lugarElegido").innerHTML = ["Plaza del Carmen"];
+      break;
+
+    case "Villa Elisa":
+      document.getElementById("lugarElegido").innerHTML = [" Parque Ecológico ", " Plaza Castells (Camino Centenario entre 49 y 51) "];
+  
+  }
 }
